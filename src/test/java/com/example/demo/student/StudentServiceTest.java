50c5ecf8bc0d12de2fb20f082881484ca7dc76da
Zdenko Bučo <buco.zdeno@gmail.com>
2023-07-09 16:03:46 +0200

    testing finished (units)

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,20 +1,28 @@
 package com.example.demo.student;
 
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 // this annotation replace all staff commented with star
 class StudentServiceTest {
+    //run studentServiceTest with Coverage - I can check the coverage of tests
     @Mock
     private StudentRepository studentRepository;
     // * private AutoCloseable autoCloseable;
@@ -41,12 +49,40 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addStudent() {
+    void canAddStudent() {
+        // given
+        String email = "jamila@gmail.com";
+        Student student = new Student("Jamila", email, Gender.FEMALE );
+        //when
+        underTest.addStudent(student);
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor =
+                ArgumentCaptor.forClass(Student.class);
+        verify(studentRepository)
+                .save(studentArgumentCaptor.capture());
+        Student capturedStudent = studentArgumentCaptor.getValue();
+        assertThat(capturedStudent).isEqualTo(student);
+    }
+
+    @Test
+    void willThrowWhenEmailIsTaken() {
+        // given
+        String email = "jamila@gmail.com";
+        Student student = new Student("Jamila", email, Gender.FEMALE );
+        given(studentRepository.selectExistsEmail(student.getEmail()))
+                .willReturn(true);
+
+        //when
+        // then
+        assertThatThrownBy(() -> underTest.addStudent(student))
+                .isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + student.getEmail() + " taken");
+        verify(studentRepository, never()).save(any());
+        //if exception is thrown, check if any student is saved in repo.
     }
 
     @Test
     @Disabled
-    void deleteStudent() {
+    void canDeleteStudent() {
     }
 }
