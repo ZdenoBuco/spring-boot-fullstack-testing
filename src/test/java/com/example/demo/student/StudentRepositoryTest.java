44192a02c6e9850efbe7fc066c91e1d591982118
Zdenko Bučo <buco.zdeno@gmail.com>
2023-07-09 14:41:45 +0200

    added mocking test to demo testing

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,17 +1,26 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
-
+@DataJpaTest
+//redirect to testing DB
 class StudentRepositoryTest {
     //DB doesn't work but this is working testing...
 
     @Autowired
     private StudentRepository underTest;
 
+    @AfterEach
+    void tearDown() {
+        underTest.deleteAll();
+        //deleting all data in DB after test
+    }
 
     @Test
     void itShouldCheckIfStudentExistsByEmail() {
@@ -24,4 +33,14 @@ class StudentRepositoryTest {
         // then
         assertThat(exists).isTrue();
     }
+    @Test
+    void itShouldCheckIfStudentDoesNotExistsByEmail() {
+        // given
+        String email = "jamila@gmail.com";
+        // when
+        Boolean exists = underTest.selectExistsEmail(email);
+        // then
+        assertThat(exists).isFalse();
+    }
+    // we're testing method .selectExistEmail because that's only method what we created/wrote down
 }
